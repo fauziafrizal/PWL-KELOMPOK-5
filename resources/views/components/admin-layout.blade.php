5ac1e68babf50aa92b17af01a50f7a4b92caf6fe
fauziafrizal <fauziafrizal150404@gmail.com>
2026-04-10 22:14:27 +0700

    membuat struktur dasar admin layout

---
+++ resources/views/components/admin-layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Panel Minimarket</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="min-h-screen bg-slate-100 text-slate-900">
+</body>
+</html>
